f50fb27b716964c49becbb3f26a56ebc0fdb0a7d
Petru Lucian Giurca <petru.giurca@pm.me>
2025-08-04 16:47:01 +0300

    [Update] .gitignore

--- app.py
+++ app.py
@@ -612,9 +612,9 @@ def main():
                 try:
                     # Define month mappings
                     months = {
-                        "january": 1, "february": 2, "march": 3, "april": 4, "August": 5, "june": 6,
+                        "january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6,
                         "july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12,
-                        "jan": 1, "feb": 2, "mar": 3, "apr": 4, "August": 5, "jun": 6,
+                        "jan": 1, "feb": 2, "mar": 3, "apr": 4, "may": 5, "jun": 6,
                         "jul": 7, "aug": 8, "sep": 9, "oct": 10, "nov": 11, "dec": 12
                     }
 
